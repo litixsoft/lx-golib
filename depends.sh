9a54a607e446d4551978aeb337860319e12dfa82
Timo Liebetrau <diiimo2k18@gmail.com>
2018-05-07 23:33:37 +0200

    chore: update db setup

--- depends.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-go get -v github.com/litixsoft/lx-golib/crypt
-go get -v github.com/stretchr/testify/assert
-go get -v github.com/globalsign/mgo
-go get -v github.com/globalsign/mgo/bson
-go get -v golang.org/x/crypto/bcrypt
-go get -v github.com/labstack/echo
-go get -v github.com/golang/mock/gomock
