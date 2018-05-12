82368b79036c8a3954bc28299f0d0e04f773ae29
Timo Liebetrau <diiimo2k18@gmail.com>
2018-05-12 20:34:32 +0200

    chore: update json fields

--- helper/types.go
+++ helper/types.go
@@ -7,11 +7,11 @@ import (
 type M map[string]interface{}
 
 type ReqByQuery struct {
-	Options lxDb.Options `json:"opts, omitempty"`
+	Options lxDb.Options `json:"opts,omitempty"`
 	Query   M            `json:"query"`
 }
 
 type ReqById struct {
 	Id string `json:"id"`
-	Data M `json:"data, omitempty"`
+	Data M `json:"data,omitempty"`
 }
