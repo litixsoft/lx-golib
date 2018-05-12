3957016ff142d471514af441fa868f55a7a7e399
Timo Liebetrau <diiimo2k18@gmail.com>
2018-05-12 13:31:10 +0200

    chore: req data

--- helper/types.go
+++ helper/types.go
@@ -1,10 +1,17 @@
 package lxHelper
 
-import "github.com/litixsoft/lx-golib/db"
+import (
+	"github.com/litixsoft/lx-golib/db"
+)
 
 type M map[string]interface{}
 
 type ReqGetData struct {
-	Options lxDb.Options `json:"opts"`
-	Query   M  `json:"query"`
+	Options lxDb.Options `json:"opts, omitempty"`
+	Query   M            `json:"query"`
+}
+
+type ReqData struct {
+	Query M `json:"query"`
+	Data  M `json:"query"`
 }
